f045b069efd5c950ba4c034fb7eb9bd3c5295e5b
abdeljalilBs <bouaraisjalil6@gmail.com>
2025-06-03 10:00:04 +0100

    update1

--- index.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.getElementById("toggle");
-console.log(toggle);
-
-const navbar = document.getElementById('navbar');
-
-toggle.addEventListener('click',()=>{
-    navbar.classList.add('othman')
-})
